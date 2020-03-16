7293e3f6ce8ed8ac5412db7fd0249965e24f01f8
Jakub <lemiszewski@gmx.com>
2020-03-16 18:17:35 +0100

    Changed order of messages

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -6,13 +6,13 @@ import Logger from './logger';
 // Initialize express
 export default async ({ expressApp }) => {
   try {
-    // Loading Mongo
-    await mongooseLoader();
-    Logger.info('✌️ DB loaded and connected! 🔋');
-
     // Loading express
     await expressLoader({ app: expressApp });
     Logger.info('✌️ Express loaded');
+
+    // Loading Mongo
+    await mongooseLoader();
+    Logger.info('✌️ DB loaded and connected! 🔋');
   } catch (error) {
     Logger.error(`${logSymbols.error} ${error}`);
   }
