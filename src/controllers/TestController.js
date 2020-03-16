c9bfb9ff3e5bd4e261ee85d14e0fbabf0bff1100
Jakub <lemiszewski@gmx.com>
2020-03-16 18:21:59 +0100

    Test to see error message

--- src/controllers/TestController.js
+++ src/controllers/TestController.js
@@ -1,14 +1,15 @@
 import Logger from '../loaders/logger';
 
 // An example of a controller for an endpoint
+// Use this only as a starting point
 const TestController = {
   async getAll(req, res, next) {
     try {
       const params = req.params.id;
       // console.log(params);
-      return await res.status(200).json({ msg: 'Hello', params });
+      return await res.status(200).json({ msg: 'Hello', id });
     } catch (err) {
-      Logger.error('🔥 Error: %o', err);
+      Logger.error('🔥', err);
       return next(err);
     }
   },
